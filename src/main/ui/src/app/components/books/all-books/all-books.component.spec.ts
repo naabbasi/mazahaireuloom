112e9ed42f98935d1b712153a6f1877d598a5059
Noman Ali <noman.ali.abbasy@gmail.com>
2023-06-20 23:21:56 +0300

    Upgraded to angular 10

--- src/main/ui/src/app/components/books/all-books/all-books.component.spec.ts
+++ src/main/ui/src/app/components/books/all-books/all-books.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AllBooksComponent } from './all-books.component';
 
@@ -6,7 +6,7 @@ describe('AllBooksComponent', () => {
   let component: AllBooksComponent;
   let fixture: ComponentFixture<AllBooksComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AllBooksComponent ]
     })
